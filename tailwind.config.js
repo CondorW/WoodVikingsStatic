591a963090aeab37aa2db6eaab84a16164536609
CondorW <rotsteiner@gmail.com>
2024-02-11 17:27:37 +0100

    Trying to fix for deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   purge: ['./src/**/*.{html,ts}'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: media,
   mode: 'jit',
   theme: {
     extend: {},
