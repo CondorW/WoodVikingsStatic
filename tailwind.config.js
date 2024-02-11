881944edb499985f36dcdfbf92154d490acfc4ba
CondorW <rotsteiner@gmail.com>
2024-02-11 16:07:27 +0100

    Static Site Finished

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./src/**/*.{html,ts}'],
+  darkMode: false, // or 'media' or 'class'
+  mode: 'jit',
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
